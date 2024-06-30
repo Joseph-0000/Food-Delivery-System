75b393df46639036f690b71e6922a8523a333dab
Joseph-0000 <140462648+Joseph-0000@users.noreply.github.com>
2024-06-30 22:38:50 +0800

    commit

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,7 +1,7 @@
 import foodModel from "../models/foodModel.js";
 import fs from 'fs'
 
-//add food
+//add food item
 const addFood = async (req,res) => {
 
     let image_filename = `${req.file.filename}`;
@@ -21,7 +21,6 @@ const addFood = async (req,res) => {
         console.log(error)
         res.json({success: false, message: "Error:"})
     }   
-
 }
 
 // all food list
--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const foodSchema = new mongoose.Schema({
     name: {type: String, required: true},
